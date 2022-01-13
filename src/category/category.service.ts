0e3a9d6045d30f694ac3665c85f608550f71c927
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-12 22:03:49 -0300

    feat(category): create `create` method on `CategoryService`

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ICategory } from './category.interface';
+import { PrismaService } from '../prisma.service';
+import { Category } from '@prisma/client';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(category: ICategory): Promise<Category> {
+    return this.prisma.category.create({ data: category });
+  }
+}
